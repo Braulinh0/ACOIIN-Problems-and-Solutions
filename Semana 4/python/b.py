9a8945159c0628a4c38366b2e23044ede5743b89
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-07-05 13:25:56 -0400

    Update b.py

--- Semana 4/python/b.py
+++ Semana 4/python/b.py
@@ -1,41 +1,14 @@
-#pragma GCC optimize("O2")
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define f float
-#define endl '\n'
+n = int(input())
 
-void usaco(string filename) {
-    freopen((filename + ".in").c_str(), "r", stdin);
-    freopen((filename + ".out").c_str(), "w", stdout);
-}
+count1 = 0
+count2 = 0
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.setf(ios::fixed);
-    cout.precision(10);
-
-    // usaco("filename");
-
-    int t;
-    cin >> t;
+for i in range(n) :
+    num = int(input())
+    count1 += num
     
-    int temp1 = 0;
-    int temp2 = 0;
-    while(t--) {
-        int num;
-        cin >> num;
-
-        temp1 += num;
-
-        if(num > 3) {
-            temp2++;
-        }
-    }
-
-    cout << "Total de tortas vendidas: " << temp1 << endl;
-    cout << "Clientes que compraron más de 3 tortas: " << temp2 << endl;
-
-    return 0;
-}
+    if num > 3 :
+        count2 += 1
+        
+print(f"Total de tortas vendidas: {count1}")
+print(f"Clientes que compraron más de 3 tortas: {count2}")
